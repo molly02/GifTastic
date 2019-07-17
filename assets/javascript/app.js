c91f8a839bc96d412521d506b1e9e29a676437d9
mp02 <molly_patterson@yahoo.com>
2019-07-16 19:21:39 -0500

    created files, folders and added initial framework

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+// link api
+//check it's working
+
+// look to past exercises
+
+// add one button first to check
+// generate only g - pg -13 gifs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})

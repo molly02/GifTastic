6b3e968208ff745bd0843e2a1dd8e4205be11f3b
mp02 <molly_patterson@yahoo.com>
2019-07-21 16:26:47 -0500

    made gifs start and stop with tutor

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,39 @@
 $(document).ready(function(){
 
 
-   $("button").on("click", function(){
-
+    $("button").on("click", function(){
+    
     var animal = $(this).attr("data-animal");
-
+    
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     animal + "&api_key=2UD0Bpu5tljRD3eZcOezMKLrWzlDv5oB&limit=10";
-
+    
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-
+    
     .then(function(response){
         console.log(queryURL);
         console.log(response);
-
-       // check pausing gits exercise, how to reference still and animated gifs? / different links? 
-       // 2 links? one for still and one for animated gifs? 
-       var results = response.data;
-
+        
+        // check pausing gits exercise, how to reference still and animated gifs? / different links? 
+        // 2 links? one for still and one for animated gifs? 
+        var results = response.data;
+        
         for (var i = 0; i < results.length; i++) {
             var animalDiv = $("<div>");
             var p = $("<p>").text("Rating: " + results[i].rating);
+            // create var that contains the still and one for animated
+            var still = results[i].images.fixed_height_still.url;
+            var animated = results[i].images.fixed_height.url;
             var animalImage = $("<img>");
 
-            animalImage.attr("src", results[i].images.fixed_height.url);
-            
+            animalImage.attr("src", still);
+            animalImage.attr("data-still", still);
+            animalImage.attr("data-animate", animated);
+            animalImage.attr("data-state", "still");
+            animalImage.addClass("animal-image");
             animalDiv.append(p);
             animalDiv.append(animalImage);
 
@@ -35,28 +41,31 @@ $(document).ready(function(){
 
         }
 
-    })
+    }) 
     
-    
-    var state = $(this).attr("data-state");
+})
 
+$(document).on("click", ".animal-image", function() {
+
+    var state = $(this).attr("data-state");
+    
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
     }
     else {
-        $(this).attr("src", $(this).attr("still"));
+        $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
     }
 
-   })
+})
 
 
 // fixed position for css
 // make all gifs go across then down
-
+// 6.2 exercise 10 for clearing after
 // look to past exercises
-
+//6.2 09 for adding button
 // have them be frozen and start moving only once clicked
 // be able to add a button and click on it as well
 // loop buttons in a string?

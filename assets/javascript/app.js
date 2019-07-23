0db218ba7b1625803898614df604862efda95b50
mp02 <molly_patterson@yahoo.com>
2019-07-22 23:46:34 -0500

    added rating cleared page after each button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,29 +16,34 @@ $(document).ready(function(){
     .then(function(response){
         console.log(queryURL);
         console.log(response);
-        
-        // check pausing gits exercise, how to reference still and animated gifs? / different links? 
-        // 2 links? one for still and one for animated gifs? 
+     
         var results = response.data;
+
+        $("#gifs-appear-here").empty();
         
         for (var i = 0; i < results.length; i++) {
-            var animalDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
-            // create var that contains the still and one for animated
-            var still = results[i].images.fixed_height_still.url;
-            var animated = results[i].images.fixed_height.url;
-            var animalImage = $("<img>");
 
-            animalImage.attr("src", still);
-            animalImage.attr("data-still", still);
-            animalImage.attr("data-animate", animated);
-            animalImage.attr("data-state", "still");
-            animalImage.addClass("animal-image");
-            animalDiv.append(p);
-            animalDiv.append(animalImage);
-
-            $("#gifs-appear-here").prepend(animalDiv);
-
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                var rating = results[i].rating;
+            
+                var animalDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                var still = results[i].images.fixed_height_still.url;
+                var animated = results[i].images.fixed_height.url;
+                var animalImage = $("<img>");
+                
+                animalImage.attr("src", still);
+                animalImage.attr("data-still", still);
+                animalImage.attr("data-animate", animated);
+                animalImage.attr("data-state", "still");
+                animalImage.addClass("animal-image");
+                animalDiv.append(p);
+                animalDiv.append(animalImage);
+                
+                $("#gifs-appear-here").append(animalDiv);
+                
+            }
+           
         }
 
     }) 
@@ -60,27 +65,6 @@ $(document).on("click", ".animal-image", function() {
 
 })
 
-
-// fixed position for css
-// make all gifs go across then down
-// 6.2 exercise 10 for clearing after
-// look to past exercises
-//6.2 09 for adding button
-// have them be frozen and start moving only once clicked
-// be able to add a button and click on it as well
-// loop buttons in a string?
-// clear current gifs and replace with new ones when new button is clicked
-
-
-// generate only g - pg -13 gifs
-
-
-
-//  Be sure to read about these GIPHY parameters (hint, hint):
-// `q`
-// `limit`
-// `rating`
-
-
+// need to add 'add button' function still
 
 })
